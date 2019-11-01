47dc36dd7d0136ba81ccf1fb1f018ce08e69f60f
Mitchell Lamosek <lamosekm@gilmour.org>
2019-11-01 13:24:04 -0400

    malloy

--- student.py
+++ student.py
@@ -132,10 +132,11 @@ class Piggy(PiggyParent):
         print("-------- [ Press CTRL + C to stop me ] --------\n")
         print("-----------! NAVIGATION ACTIVATED !------------\n")
         print("Wait a second. \nI can't navigate the maze at all. Please give my programmer a zero.")
-        while self.read_distance() > 250:
-            self.fwd()
-            time.sleep(.01)
-        self.stop()
+        while True:    
+            while self.read_distance() > 250:
+                self.fwd()
+                time.sleep(.01)
+            self.turn_by_deg(45)
 
     def doCircles(self):
         """does two full circles"""
