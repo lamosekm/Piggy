334ee3f5cb3b6953ce69ce7a29e8b3679b5b11a9
Mitchell Lamosek <lamosekm@gilmour.org>
2019-11-06 10:50:44 -0500

    25

--- student.py
+++ student.py
@@ -105,7 +105,7 @@ class Piggy(PiggyParent):
 
     def scan(self):
         """Sweep the servo and populate the scan_data dictionary"""
-        for angle in range(self.MIDPOINT-350, self.MIDPOINT+350, 15):
+        for angle in range(self.MIDPOINT-350, self.MIDPOINT+350, 25):
             self.servo(angle)
             self.scan_data[angle] = self.read_distance()
 
