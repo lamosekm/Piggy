57c7e5493d7b541307b647f925e354d14561b535
Mitchell Lamosek <lamosekm@gilmour.org>
2019-12-16 11:31:57 -0500

    fixed tabbing

--- student.py
+++ student.py
@@ -185,32 +185,32 @@ class Piggy(PiggyParent):
 
     def slither(self):
         """ practice a smooth veer """
-    # write down where we started
-    starting_direction = self.get_heading()
-    # start driving forward
-    self.set_motor_power(self.MOTOR_LEFT, self.LEFT_DEFAULT)
-    self.set_motor_power(self.MOTOR_RIGHT, self.RIGHT_DEFAULT)
-    self.fwd()
-    # throttle down the left motor
-    for power in range(self.LEFT_DEFAULT, 30, -10):
-        self.set_motor_power(self.MOTOR_LEFT, power)
-        time.sleep(.5)
-    # throttle up the left 
-    for power in range(30, self.LEFT_DEFAULT + 1, 10):
-        self.set_motor_power(self.MOTOR_LEFT, power)
-        time.sleep(.1)
+        # write down where we started
+        starting_direction = self.get_heading()
+        # start driving forward
+        self.set_motor_power(self.MOTOR_LEFT, self.LEFT_DEFAULT)
+        self.set_motor_power(self.MOTOR_RIGHT, self.RIGHT_DEFAULT)
+        self.fwd()
+        # throttle down the left motor
+        for power in range(self.LEFT_DEFAULT, 30, -10):
+            self.set_motor_power(self.MOTOR_LEFT, power)
+            time.sleep(.5)
+        # throttle up the left 
+        for power in range(30, self.LEFT_DEFAULT + 1, 10):
+            self.set_motor_power(self.MOTOR_LEFT, power)
+            time.sleep(.1)
 
-    # throttle down the right motor
-    for power in range(self.RIGHT_DEFAULT, 30, -10):
-        self.set_motor_power(self.MOTOR_RIGHT, power)
-        time.sleep(.5)
-    # throttle up the right 
-    for power in range(30, self.RIGHT_DEFAULT + 1, 10):
-        self.set_motor_power(self.MOTOR_RIGHT, power)
-        time.sleep(.1)
+        # throttle down the right motor
+        for power in range(self.RIGHT_DEFAULT, 30, -10):
+            self.set_motor_power(self.MOTOR_RIGHT, power)
+            time.sleep(.5)
+        # throttle up the right 
+        for power in range(30, self.RIGHT_DEFAULT + 1, 10):
+            self.set_motor_power(self.MOTOR_RIGHT, power)
+            time.sleep(.1)
 
-    left_speed = self.LEFT_DEFAULT
-    right_speed = self.RIGHT_DEFAULT
+        left_speed = self.LEFT_DEFAULT
+        right_speed = self.RIGHT_DEFAULT
 
 
     # straighten out
